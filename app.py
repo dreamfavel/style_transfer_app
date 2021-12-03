7fa1d8a0745335b43162684041bb27e12a9395f9
adaniky <paladdick@gmail.com>
2021-12-03 23:38:40 +0200

    fix image show

--- app.py
+++ app.py
@@ -11,28 +11,23 @@ def index():
 
 @app.route('/', methods=['POST'])
 def content_img():
-    print('USE FIRST FUNC')
     content_img = request.files.get('content_img')
     if content_img:
         filename =  os.getcwd() + '/static/content_img.jpg'
         filename2 = 'content_img.jpg'
         content_img.save(filename)
-        return render_template('template.html', filename_content = filename2)
+        return render_template('template.html', filename = filename2)
     else:
-        return render_template('template.html')
+        style_img = request.files.get('style_img')
+        if style_img:
+            filename =  os.getcwd() + '/static/style_img.jpg'
+            filename2 = 'style_img.jpg'
+            style_img.save(filename)
+            return render_template('template.html', filename = filename2)
+        else:
+            return render_template('template.html')
 
-@app.route('/', methods=['POST'])
-def style_img():
-    print(request.files)
-    print('SOME')
-    style_img = request.files.get('style_img')
-    if style_img:
-        filename =  os.getcwd() + '/static/style_img.jpg'
-        filename2 = 'style_img.jpg'
-        style_img.save(filename)
-        return render_template('template.html', filename_style = filename2)
-    else:
-        return render_template('template.html')
+
 
 
 
