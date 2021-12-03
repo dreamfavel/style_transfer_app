251e269ff0962a467eb63793aba7c71f04ac211a
adaniky <paladdick@gmail.com>
2021-12-03 21:25:37 +0200

    init2

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask, # request, redirect, url_for, send_from_directory,
-                  render_template
+from flask import Flask, render_template
+ # request, redirect, url_for, send_from_directory,
+
 
 app = Flask(__name__)
 
