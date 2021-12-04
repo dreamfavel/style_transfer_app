f00bbf4b4e7cd5e1b642180bd8fe5565eab3c51a
adaniky <paladdick@gmail.com>
2021-12-04 11:29:48 +0200

    clean previos predictions

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import os
+import shutil
 from flask import Flask, render_template, request
 from utils import tensor_to_image, load_img
 from utils import create_stylized_image, hub_module
 
+STATIC_DIR =  os.getcwd() + '/static/'
+if not os.path.exists(STATIC_DIR):
+    os.makedirs(STATIC_DIR)
 
 app = Flask(__name__)
 
@@ -14,20 +18,28 @@ def index():
 def upload_images():
 
     if request.method == 'POST':
+        # clean previos predictions
+        for filename in os.listdir(STATIC_DIR):
+            file_path = os.path.join(STATIC_DIR, filename)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+            except Exception as e:
+        print('Failed to delete %s. Reason: %s' % (file_path, e))
         # get content image from request
         content_img = request.files.get('content_img')
-        filename1 =  os.getcwd() + '/static/content_img.jpg'
+        filename1 =  STATIC_DIR + 'content_img.jpg'
         content_img.save(filename1)
         # get style image from request
         style_img = request.files.get('style_img')
-        filename2 =  os.getcwd() + '/static/style_img.jpg'
+        filename2 =  STATIC_DIR + 'style_img.jpg'
         style_img.save(filename2)
 
         # transform images to the required format
         content_img = load_img(filename1)
         style_img = load_img(filename2)
         # create path to predicted image
-        img_path = os.getcwd() + '/static/predict_img.jpg'
+        img_path = os.getcwd() + 'predict_img.jpg'
         # predict and save image with transfered style
         create_stylized_image(hub_module, content_img, style_img, img_path)
 
